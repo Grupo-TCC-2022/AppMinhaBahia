cf3ce8917e57f6d91be63540687534781da8d71d
wendelsimoes <wendel.simoes.br@gmail.com>
2022-06-07 23:50:34 -0300

    Criar projeto e adicionar gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppMinhaBahia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
